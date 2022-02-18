6fb3e5f2c8cce5fc53f64b54482313565bd76707
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-18 16:27:02 +0100

    Changes to photo upload

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  has_many_attached :photos
 end
